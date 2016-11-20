b1668990281e625c666265338b07abe66fe6d1ac
Rostyslav Kurdiumov <rkurdium@linux-kcvp.suse>
2016-11-20 22:51:57 +0100

    Navbar changed

--- js/smooth-scrolling.js
+++ js/smooth-scrolling.js
@@ -2,10 +2,5 @@ $(document).ready(function(){
     $("nav a").click(function(evn){
         evn.preventDefault();
         $('html,body').scrollTo(this.hash, this.hash); 
-
-        $("ul.nav li").removeClass("active");
-        $(this).parent().addClass("active") 
     });   
-
-
 });
